d865c2fea7faf05a4538b168aaf78c5fa6ce30cf
Andrey Amaral <amaralandrey@gmail.com>
2026-02-14 23:43:13 -0300

    corrige try except

--- condicionais/questao_01.py
+++ condicionais/questao_01.py
@@ -2,25 +2,26 @@
 1) Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
 """
 
-values = []
+values = [] # os índices representam a ordem alfabética.
 labels = ["A", "B", "C"]
 
-for i in range(3):
-    try:
-        valor = int(input(f"Informe o valor de {labels[i]}: "))
-        values.append(valor)
-    except ValueError:
-        print("Valor inválido!")
-        valor = int(input(f"Informe o valor de {labels[i]}: "))
-        values.append(valor)
-
-def greater_than_c(array):
+def less_than_c(array):
+    """ Esta função avalia se a soma de A + B é menor que C """
     if array[0] + array[1] < array[2]:
         return True
     else:
         return False
+
+i = 0
+while i < 3:
+    try:
+        valor = int(input(f"Informe o valor de {labels[i]}: "))
+        values.append(valor)
+        i += 1
+    except ValueError:
+        print("Valor inválido!")
     
-if greater_than_c(values) == True:
-    print(f"A + B ({values[0]} + {values[1]}) é menor que C ({values[2]})")
+if less_than_c(values) == True:
+    print(f"A + B ({values[0]} + {values[1]}) é menor que C ({values[2]}).")
 else:
-    print(f"A + B ({values[0]} + {values[1]}) é maior que C ({values[2]})")
+    print(f"A + B ({values[0]} + {values[1]}) não é menor que C ({values[2]}).")
